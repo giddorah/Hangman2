cf8818d03fa73fbf7fdf5d237dc9cad342e7cde3
Marc Uddenfeldt <Marcuddenfeldt@hotmail.com>
2016-09-13 14:24:57 +0200

    Ändrade till ett litet j

--- Hangman2/Program.cs
+++ Hangman2/Program.cs
@@ -174,7 +174,7 @@ namespace Hangman2
         private static void WordGenerator() // Metod för att välja ord från ordlistan.
         {
             Console.WriteLine("WordGenerator");
-            wordGeneratorWord = "Johan";
+            wordGeneratorWord = "johan";
             
         }
 
