ba611d215a66b9a36efe12772fc181d9021a2e7e
Marc Uddenfeldt <Marcuddenfeldt@hotmail.com>
2016-09-13 15:32:46 +0200

    Har fixat så man ser gissningar

--- Hangman2/Program.cs
+++ Hangman2/Program.cs
@@ -157,9 +157,11 @@ namespace Hangman2
 
         private static void Guess() // Metod för att samla in gissning från spelaren.
         {
+            Console.WriteLine("Du har gissat på:" + guessHistory);
             WordLength();
             Console.WriteLine("Gissa ord: ");
             playerGuess = Console.ReadLine().ToLower();
+            History();
             CompareWord();
             
             
@@ -191,7 +193,7 @@ namespace Hangman2
 
         private static void History() // Metod för att visa historiken över gissade ord.
         {
-            Console.WriteLine("History");
+            guessHistory += " " + playerGuess;
         }
 
         private static void Lives() // Metod för att hålla reda på antal liv.
