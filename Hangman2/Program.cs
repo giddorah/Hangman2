fbb230c75531cb0abcb680122644d88acd5ed194
Marc Uddenfeldt <Marcuddenfeldt@hotmail.com>
2016-09-14 09:43:19 +0200

    gjorde fel men nu är det rätt med ordet

--- Hangman2/Program.cs
+++ Hangman2/Program.cs
@@ -244,7 +244,7 @@ namespace Hangman2
         {
 
             playerLives = 4;
-            wordGeneratorWord = "Kungen";
+            wordGeneratorWord = "kungen";
             numberOfGuesses = 0;
             guessHistory = "";
             Guess();
