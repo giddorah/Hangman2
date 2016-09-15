e27dac622282a8c1aa60735b2cd7b4bbc49a0b0c
Michlewood <michlewood@hotmail.com>
2016-09-15 18:53:04 +0200

    fixade de olika felen i CompareWord som t ex att man inte kunde vinna. flyttade en del kod från CompareWord till GameInterface. skrev beskrivningar på det mesta kan alltid tas bort om det är för klottrigt.

--- Hangman2/Program.cs
+++ Hangman2/Program.cs
@@ -12,12 +12,15 @@ namespace Hangman2
         static int playerLives; // Lagrar spelarens antal liv
         static string playerName; // Lagrar spelarens namn
         static string playerGuess; // Lagrar spelaerns gissning
-        static string wordGeneratorWord; // Lagrar ordgeneratorns ord
+        static string[] wordGeneratorWord; // Lagrar ordgeneratorns ord som en array av stringar
+        static string theWord;  //  Lagrar ordgeneratorns ord som en string
         static int wordLength; // Lagrar ordets antal bokstäver
         static string guessHistory; // Lagrar tidigare gissningar
         static int numberOfGuesses; // Lagrar hur många gissningar som gjorts
-        static int menuSwitchHowTo;
-        static char[] maskedWord;
+
+        //en array av strings som är lika långt som wordGeneratorWord men har bara understreck i början
+        //och kommer att fyllas ut med bokstäver under spelets gång
+        static string[] maskedWord; 
         #endregion
 
         static void Main(string[] args)
@@ -58,9 +61,10 @@ namespace Hangman2
             {
 
                 Console.WriteLine("Skriv in namn.");
-                playerName = Console.ReadLine();
+                playerName = Console.ReadLine(); //tar spelarens namn
 
-                if (playerName.Length >= 3 && playerName.Length <= 25)
+                //namnet måste vara mellan 3 och 25 bokstäver annars kommer den inte att accepteras och loopen kommer att köras igen
+                if (playerName.Length >= 3 && playerName.Length <= 25) 
                 {
                     Console.WriteLine("välkommen " + playerName);
                     MainMenu();
@@ -76,15 +80,23 @@ namespace Hangman2
 
         private static void MainMenu() // Huvudmenyn till spelet.
         {
-            //bool menuLoop = true; // En loop för att huvudmenyn ska finnas tillgänglig så länge spelaren befinner sig utanför spelet.
-            int mainMenuSwitch;
+            //variabel som tar det som spelaren skriver och sparar det som en string 
             string checkInput;
+
+            //variabel som kommer att bestämma vilket val som görs i switchen den kommer att tilldelas
+            // ett värde från checkInput efter att det har försäkrats att det är ett nummer
+            int mainMenuSwitch;
+
+            // En loop för att huvudmenyn ska finnas tillgänglig så länge spelaren befinner sig utanför spelet.
             while (true)
             {
                 Console.Clear(); // Rensar konsollen från tidigare kommandon.
                 MainMenuGui(); // Visar gränssnittet för MainMenu.
+
                 checkInput = Console.ReadLine();
 
+                //jämfr det spelaren har skrivit, det måste vara 1, 2 eller 3 annars kommer det inte accepteras och
+                //spelaren måste skriva om
                 if (checkInput.Equals("1") || checkInput.Equals("2") || checkInput.Equals("3"))
                 {
                     mainMenuSwitch = int.Parse(checkInput);
@@ -92,6 +104,8 @@ namespace Hangman2
 
                 else
                 {
+                    //när mainMenuSwitch får vädet noll så kommer den att åka till default
+                    //och när den är klar där så kommer användaren frågas om igen eftersom allt ligger i en while-loop
                     mainMenuSwitch = 0;
                 }
 
@@ -124,7 +138,12 @@ namespace Hangman2
 
         private static void Difficulty()
         {
-            while (true)
+            //orden som kan väljas mellan spara som string arrayer istället för stringar så att vi ska ha mer kontroll över dem
+            string[] easy = new string[] {"k", "u", "n", "g", "e", "n"};
+            string[] medium = new string[] {"a","s","k","u","n","g","e","n"};
+            string[] hard = new string[] {"k","o","n","t","i","n","e","n","t","a","l"};
+
+            while (true) // En loop som körs tills spelaren har valt ett giltigt val funkar som i huvudmenyn
             {
                 int difficultySwitch;
 
@@ -132,14 +151,6 @@ namespace Hangman2
                 Console.WriteLine("Vill du köra på lätt nivå? Tryck 1:");
                 Console.WriteLine("Vill du köra på medel nivå? Tryck 2:");
                 Console.WriteLine("Vill du köra på svår nivå? Tryck 3:");
-
-                string[] easy = new string[1];
-                string[] medium = new string[1];
-                string[] hard = new string[1];
-
-                easy[0] = "kungen";
-                medium[0] = "askungen";
-                hard[0] = "kontinental";
 
                 string checkInput = Console.ReadLine();
                 int inputCheckLength = checkInput.Length;
@@ -156,25 +167,28 @@ namespace Hangman2
                     difficultySwitch = 0;
 
                 }
-
+                //WordGeneratorWord sätts det ord som motsvarar den svårighet spelaren valt
+                //och sedan anropar den wordGenerator
                 switch (difficultySwitch)
                 {
                     case 1:
                         Console.WriteLine("Du valde lätt nivå!");
-                        wordGeneratorWord = easy[0];
+                        wordGeneratorWord = easy;
                         WordGenerator();
-
                         return;
+
                     case 2:
                         Console.WriteLine("Du valde medel nivå");
-                        wordGeneratorWord = medium[0];
+                        wordGeneratorWord = medium;
                         WordGenerator();
                         return;
+
                     case 3:
                         Console.WriteLine("Du valde svår nivå");
-                        wordGeneratorWord = hard[0];
+                        wordGeneratorWord = hard;
                         WordGenerator();
                         return;
+
                     default:
                         Console.WriteLine("Var vänlig och skriv in 1,2 eller 3");
                         break;
@@ -184,20 +198,29 @@ namespace Hangman2
 
         private static void WordGenerator() // Metod för att välja ord från ordlistan.
         {
-
             Console.Clear();
+
+            //här görs lite förberedelser inför spelet, återställer värdena om spelet körts innan
             playerLives = 4;
             numberOfGuesses = 0;
             guessHistory = "";
             wordLength = wordGeneratorWord.Length;
-            maskedWord = new char[wordLength];
+            theWord = "";
 
+            //här tilldelas stringen theWord så att den får samma ord som wordGeneratorWord
+            //skillnaden är att theWord är en string och wordGeneratorWord är en array av stringar
             for (int i = 0; i < wordLength; i++)
-
             {
+                theWord += wordGeneratorWord[i];
+            }
 
-                maskedWord[i] = '_';
+            //maskedWord får samma längd som wordGeneratorWord
+            maskedWord = new string[wordLength];
 
+            //maskedWord får ett understreck motsvarande varje bokstav dessa kommer bytas ut mot bokstäver under spelets gång
+            for (int i = 0; i < wordLength; i++)
+            {
+                maskedWord[i] = "_";
             }
             GameLoop();
         }
@@ -212,27 +235,53 @@ namespace Hangman2
                 History();
 
 
-                //metoden CompareWord Anropas och om den är sann och då går man ur loopen.
-                //den är sann om man har vunnit eller förlorat som kollas i metoden
-                if (CompareWord()) return;
+                //metoden CompareWord Anropas och om den är sann och då går man ur loopen annars körs loopen igen.
+                //den är sann om man har vunnit eller förlorat som kollas i metoden CompareWord som returner ett bool värde
+                if (CompareWord() == true) return;
 
             }
         }
 
         private static void GameInterface() // Visar "gränssnittet" för spelaren.
         {
+            //om spelaren gör har mindre än 4 liv händer det i if satsen som gör att liven blir röda
+            //annars skrivs det ut med grå som allt annat
+            if (playerLives < 4) 
+            {
+
+
+
+                Console.Write("\nDu har: ");
+                Console.ForegroundColor = ConsoleColor.Red; //här blir det rött
+                Console.Write(playerLives);                 //här skrivs det liven
+                Console.ResetColor();                       //här återsälls färgen
+                Console.Write(" liv kvar ");
+                //Console.Beep();
+                //Console.Beep();
+                //Console.Beep();
+
+            }
+            else
+            {
+
+                Console.WriteLine("\nDu har: " + playerLives + " liv kvar");
+
+            }
+
             Console.WriteLine("\nDu har gissat på:" + guessHistory);
             WordLength();
         }
 
         private static void WordLength() // Metod för att ta reda på nuvarande ords antal bokstäver.
         {
-            wordLength = wordGeneratorWord.Length;
+            
             Console.Write("Ordet är: ");
-            for (int i = 0; i < wordLength; i++)
+            for (int i = 0; i < wordLength; i++) 
 
             {
-                Console.Write(maskedWord[i]);
+                //här skrivs bokstäverna maskedWord ut i början kommer det att bara vara understreck
+                //men senare kommer det att bytas ut mot bokstäver
+                Console.Write(maskedWord[i]);  
 
             }
 
@@ -243,84 +292,96 @@ namespace Hangman2
         {
 
             Console.Write("Gissa ord: ");
+
+            //spelaren skriver in sin gissning och det sparas som en variabel
             playerGuess = Console.ReadLine().ToLower();
+
+            //antalet gissningar ökar med 1
+            numberOfGuesses++;
         }
 
         private static void History() // Metod för att visa historiken över gissade ord.
         {
-            if (guessHistory.Equals(""))
+            //här läggs spelarens gissning i en string med alla tidigare gissningar så att de kan vissas för spelaren
+            //om historien är tom dvs när spelaren gör sin första gissning så läggs gissningen bara in
+            //senare läggs de in med ett ,-tecken mellan
+            if (guessHistory.Equals(""))  
             {
                 guessHistory += " " + playerGuess;
             }
             else guessHistory += ", " + playerGuess;
         }
 
+        //metoden skickar tillbaka ett bool värde dvs true eller false
+        //om spelaren har vunnit eller förlorat så skickas true och då kommer spelet avslutas
         private static bool CompareWord() // Metod för att jämföra Guess med WordGenerator.
         {
+            //Här tas all text bort från konsolen så att det ska inte vara så mycket störande text.
+            //anledningen att det görs här och inte i början eller slutet av gameloop är att denna metod skriver
+            //text som ska synas så det måste vara med i nästa gameloop
+            Console.Clear();
+
+            //variabel skapas för att för att kolla om man hade rätt
             bool changeMade = false;
 
+            //en for-loop som går igenom ordet bokstav för bokstav och om ett stämmer så kommer motsvarande plats i maskedWord 
+            //updateras med den gissade bokstaven. maskedWord kommer att då se mer och mer ut som  wordGeneratorWord
+            //dvs det rätta ordet. changeMade blir true för att visa att en ändring har hänt
+            //om en ändring aldrig har hänt dvs att gissningen inte stämmer överäns med någon bokstav i ordet
+            //så kommer changeMade aldrig ändras till true och skickas vidare som falsk
             for (int i = 0; i < wordLength; i++)
             {
-                if (playerGuess[0].Equals(wordGeneratorWord[i]))
+                if (playerGuess.Equals(wordGeneratorWord[i]))
                 {
-                    maskedWord[i] = playerGuess[0];
+                    maskedWord[i] = playerGuess;
                     changeMade = true;
                 }
 
             }
-            if (changeMade == false)
-            {
-                playerLives--;
-            }
+            
+            //om changeMade är falsk så innebär det att ingen ändring har hänt och då minskas ens liv 
 
-            Console.Clear();
+
+            //variabel skapas for att se om maskedWord är samma som wordGeneratorWord
+            //den sätts som true i början och om något fel hittas så kommer den bli false
             bool isEqual = true;
 
+            //en for-loop som går igenom maskedWord och wordGeneratorWord bokstav för boksvar och ser om de är lika
+            //om vid något tillfälle en bokstav i maskedWord inte är samma som motsvarande bokstav i wordGeneratorWord
+            //så får isEqual false som värde
             for (int i = 0; i < wordGeneratorWord.Length; i++)
             {
                 if (maskedWord[i] != wordGeneratorWord[i]) isEqual = false;
             }
 
-            numberOfGuesses++;
-            if (playerGuess.Equals(wordGeneratorWord) || isEqual)
+
+            //kollar om antingen spelarens gissning är samma som ordet eller om isEqual är sann vilket innebär att
+            //spelaren har gissat all bokstäver i ordet
+            //om något av dessa är sann så vinner man
+            Console.WriteLine(theWord);
+            if (playerGuess.Equals(theWord) || isEqual)
             {
                 WinGame();
                 return true;
             }
-
+            
+            //om spelarens liv är noll så förlorar man
             if (playerLives == 0)
             {
                 LoseGame();
                 return true;
             }
 
-            if (playerLives < 4)
+            if (changeMade == false)
             {
                 Console.WriteLine("\nDu gissade fel, försök igen!");
-
-
-                Console.Write("\nDu har: ");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playerLives);
-                Console.ResetColor();
-                Console.Write(" liv kvar ");
-                Console.Beep();
-                Console.Beep();
-                Console.Beep();
-
-            }
-            else
-            {
-
-                Console.WriteLine("\nDu gissade fel, försök igen!");
-                Console.WriteLine("\nDu har: " + playerLives + " liv kvar");
-
+                playerLives--;
             }
 
             return false;
         }
 
-        private static void WinGame() // Avslutar spelet beroende på hur spelaren presterat.
+        private static void WinGame() // anropas om spelaren har vunnit 
         {
             Console.WriteLine("Grattis. Du är awesome!.");
             Console.ReadLine();
@@ -345,7 +406,7 @@ namespace Hangman2
 
         private static void HowTo() // Metod för att visa HowTo'n.
         {
-            while (true)
+            while (true) //loop som funkar som loopen i huvudmenyn
             {
                 HowToGui(); // Visar gränssnittet för HowTo'n.
                 Console.WriteLine("Var vänlig och skriv in 1 eller 2");
@@ -370,7 +431,7 @@ namespace Hangman2
                 switch (menuSwitchHowTo) // Ytterligare en switchmeny men utan loop eftersom att menyn inte behöver visas igen.
                 {
                     case 1:
-                        // Låter spelaren återgå till mainmenu utan att ändra stringen för spelarnamn.
+                        // Låter spelaren återgå till mainmenu
                         return;
 
 
@@ -452,6 +513,8 @@ namespace Hangman2
             Console.WriteLine("         ```.--.                                                   ++-          ");
             Console.Write("         ```.-+-`");
 
+            //for-looparna raknar ut hur många mellanrum som ska skrivas ut innan och efter texten eftersom den
+            //varierar beroende på storleken av spelarens namn och skulle annars förstöra rammen 
             for (int i = 0; i < 12 - playerName.Length / 2; i++)
             {
                 Console.Write(" ");
